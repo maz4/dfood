07a0d5a5899d8e338ee2e60490f6a16271a2972e
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2025-12-06 18:32:02 +0900

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: process.env.GITHUB_PAGES === "true" ? "export" : undefined,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
